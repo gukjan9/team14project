cc78b9a8007a49e3ed86688fab7d9f1603846167
GukJang <9ukjang@gmail.com>
2023-09-25 19:31:38 +0900

    ci cd 테스트

--- src/main/java/com/sparta/team14project/user/signup/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/team14project/user/signup/dto/SignupRequestDto.java
@@ -18,7 +18,7 @@ public class SignupRequestDto {
     private String username;
 
     @NotBlank
-    @Pattern(regexp = "^[A-Za-z0-9@#$%^&+=!]{8,15}$", message = "비밀번호는 대소문자, 숫자, 특수문자(@#$%^&+=!)로만 구성된 8~15자의 문자열이어야 합니다.")
+    @Pattern(regexp = "^[0-9]{0,4}$", message = "비밀번호는 대소문자, 숫자, 특수문자(@#$%^&+=!)로만 구성된 8~15자의 문자열이어야 합니다.")
     private String password;
 
     @NotBlank
